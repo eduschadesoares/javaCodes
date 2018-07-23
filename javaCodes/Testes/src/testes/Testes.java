40bc964ba819135fc705b737d7b2f8aadf50ecfd
Eduardo <eduschadesoares@hotmail.com>
2018-07-23 20:26:49 -0300

    List

--- javaCodes/Testes/src/testes/Testes.java
+++ javaCodes/Testes/src/testes/Testes.java
@@ -1,13 +1,29 @@
-
 package testes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Testes {
 
+    static List nomes = new ArrayList();
+
     public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            leNome();
+        }
         
-        
-        System.out.println("Hello");
-        
+        for(Object obj : nomes) {
+            System.out.println(obj);
+        }
     }
-    
+
+    static void leNome() {
+        String nome;
+        System.out.printf("Insira um nome: ");
+        Scanner ler = new Scanner(System.in);
+        nome = ler.nextLine();
+        nomes.add(nome);
+
+    }
 }
