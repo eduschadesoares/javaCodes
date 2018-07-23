ea7099093d6ee625ddd152fc4e70b198af972717
Eduardo <eduschadesoares@hotmail.com>
2018-07-23 20:36:06 -0300

    Estudante Class

---
+++ javaCodes/Testes/src/testes/Estudante.java
@@ -0,0 +1,39 @@
+package testes;
+
+public class Estudante {
+
+    private String nome;
+    private String email;
+    private int RA;
+
+    public Estudante(String nome, String email, int RA) {
+        this.nome = nome;
+        this.email = email;
+        this.RA = RA;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRA() {
+        return RA;
+    }
+
+    public void setRA(int RA) {
+        this.RA = RA;
+    }
+
+}
