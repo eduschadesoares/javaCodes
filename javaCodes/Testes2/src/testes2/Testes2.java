6ce47cbea329feea3e2826b8c9df754a2c7fc63c
Eduardo <eduschadesoares@hotmail.com>
2018-07-24 13:07:55 -0300

    Testes 2

--- javaCodes/Testes2/src/testes2/Testes2.java
+++ javaCodes/Testes2/src/testes2/Testes2.java
@@ -3,6 +3,15 @@ package testes2;
 public class Testes2 {
 
     public static void main(String[] args) {
+        test();
+    }
+
+    static void test() {
+        String a = "1234567890";
+        int b;
+        
+        b = Integer.parseInt(a);
+        System.out.println(b);
         
     }
 
